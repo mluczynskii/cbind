115ca8c959cab1e0a882b395019799af4971a8f6
Jarek Kadecki <332771@uwr.edu.pl>
2024-04-21 16:03:49 +0200

    introducing parser of intermediate language tree

---
+++ parser/parse_functions.py
@@ -0,0 +1,96 @@
+import re
+import os
+
+def get_name(l):
+    return l[l.find(" ", 11) + 1:l.find(" ", 12)]
+
+def peek_line(f):
+    pos = f.tell()
+    line = f.readline()
+    f.seek(pos)
+    return line
+
+def skip_to_nodes(f):
+    while(not re.search("^@", peek_line(f))):
+        f.readline()
+
+def skip_empty(f):
+    while(peek_line(f) == "\n"):
+        f.readline()
+
+def eof(f, size):
+    #print(f.tell(), size)
+    return f.tell() == size
+
+def get_file_size(file):
+    pos = file.tell()
+    file.seek(0, os.SEEK_END)
+    size = file.tell()
+    file.seek(pos)
+    return size
+
+def parse_dict(d):
+    for k in d.keys():
+        vals = d[k]
+        for i in range(len(vals)):
+            if(re.search("^@", vals[i])):
+                vals[i] = d[vals[i]][0]
+    return d
+
+def retrieve_field(d, node_id, field):
+        return d[node_id][d[node_id].index(field) + 1]
+
+def retrieve_node(d, node_id):
+    return d[node_id][0]
+
+def resolve_path(d, start, path):
+    temp = start
+    for field in path:
+        temp = retrieve_field(d, temp, field)
+    if temp[0] == '@':
+        temp = retrieve_node(d, temp)
+    return temp
+
+def get_type_ids(d, node):
+    res = []
+    for k in d.keys():
+        if d[k][0] == node:
+            res.append(k)
+    return res
+
+def simplify_dict(d):
+    new_dict = {}
+    new_dict["name"] = d["@0"][0]
+    new_dict["return_expr"] = {
+        "type": resolve_path(d, get_type_ids(d, "return_expr")[0], ["expr:", "type:"]),
+        "type_name": resolve_path(d, get_type_ids(d, "return_expr")[0], ["expr:", "type:", "name:", "name:", "strg:"]),
+        }
+    new_dict["args"] = []
+    for k in get_type_ids(d, "parm_decl"):
+        new_dict["args"].append({
+            "name": resolve_path(d, k, ["name:", "strg:"]),
+            "type": d[retrieve_field(d, k, "type:")][0],
+            "type_name": resolve_path(d, k, ["type:", "name:", "name:", "strg:"])
+            })
+    return new_dict
+
+def run_parser(file):
+    file_size = get_file_size(file)
+    skip_empty(file)
+    res = []
+    while(re.search("^;; Function", peek_line(file))):
+        dict = {}
+        name = get_name(file.readline())
+        dict["@0"] = [name]
+        skip_to_nodes(file)
+        while(not (re.search("^;; Function", peek_line(file)) or eof(file, file_size))):
+            node = file.readline()
+            while(not (re.search("^@|^;; Function", peek_line(file)) or peek_line(file) == "\n")):
+                node += file.readline()
+                skip_empty(file)
+            node = node.split()
+            skip_empty(file)
+            dict[node[0]] = node[1:]
+        # print(simplify_dict(dict))
+        res.append(simplify_dict(dict))
+    return res
---
+++ parser/parser.py
@@ -0,0 +1,40 @@
+import argparse
+import json
+from parse_functions import *
+
+parser = argparse.ArgumentParser(
+    prog="parser",
+    description="Parser of intermediate language tree."
+    )
+
+parser.add_argument("-o", "--output", help="name of the output file")
+parser.add_argument("-v", "--verbose", action="store_true", help="output result into terminal")
+parser.add_argument("--no-dump", action="store_true", help="do not produce output file")
+parser.add_argument("filename")
+args = parser.parse_args()
+
+file_name = args.filename
+res = None
+try:
+
+    file = open(file_name, "r")
+    res = json.dumps(run_parser(file))
+    if args.verbose:
+        print(res)
+    file.close()
+
+except:
+    print("Could not open:", file_name)
+
+if not args.no_dump:
+    try:
+        file_name = "data.json"
+        if args.output != None:
+            file_name = args.output
+        file = open(file_name, "w")
+
+        file.write(res)
+        file.write("\n")
+        file.close()
+    except:
+        print("Could not produce output file")

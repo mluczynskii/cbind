64305ad5979cbb8cc5afe8e68f6be679ef4462f1
mluczynskii <mateusz.luczynski02@gmail.com>
2024-04-30 12:38:09 +0200

    Some refactoring

--- src/binding-generator/binding-generator.py
+++ src/binding-generator/binding-generator.py
@@ -7,151 +7,157 @@ HEADERS = [
     "lua.h",
     "lualib.h",
     "lauxlib.h"
-];
+]
 
-callbacks = {};
+usedWrapperNames = {}
 
-def load_info(filename):
-    f = open(filename, 'r');
-    data = json.load(f);
-    f.close();
-    return data;
+def loadInfo(filename):
+    f = open(filename, 'r')
+    data = json.load(f)
+    f.close()
+    return data
 
-def write_headers(file):
-    for header in [Include(h, True) for h in HEADERS]:
-        file.write(str(header) + '\n'); 
-    file.write('\n');
+def writeHeaders(file):
+    for header in [Include(lib, True) for lib in HEADERS]:
+        file.write(f'{header}\n') 
+    file.write('\n')
 
-def write_boilerplate(file):
-    file.write(boilerplate.code + '\n');
+def writeBoilerplate(file):
+    file.write(boilerplate.code + '\n')
 
-def write_declarations(file, data):
+def writeDeclarations(file, data):
     for function in data:
         arglist = []
         for i, arg in enumerate(function["args"]):
             if arg["type"] == 'fptr_type':
+                info = arg["info"]
                 fptr = FunctionPointer(
-                    arg["info"]["return_expr"]["type_name"],
+                    info["return_expr"]["type_name"],
                     f'arg{i+1}',
-                    [x["type_name"] for x in arg["info"]["args"]]
-                );
-                arglist = arglist + [str(fptr)];
+                    [x["type_name"] for x in info["args"]]
+                )
+                arglist.append(str(fptr))
             else:
-                arglist = arglist + [f'{arg["type_name"]} arg{i+1}']
+                arglist.append(f'{arg["type_name"]} arg{i+1}')
 
-        f = Function(
+        declaration = Function(
             function["return_expr"]["type_name"],
             function["name"],
             arglist, 
             modifier=Modifier.EXTERN
-        );
+        )
 
-        file.write(str(f) + '\n');
+        file.write(f'{declaration}\n');
     file.write('\n');
 
-def write_context(file):
-    context = Struct(
+def writeContext(file):
+    context = Context(
         'context',
         [Variable('lua_State*', 'stack'),
          Variable('int', 'idx')]
-    );
-    file.write(str(context) + '\n\n');
-    
-    declaration = Variable('struct context', 'c');
-    file.write(str(declaration) + '\n\n');
+    )
+    declaration = Variable('struct context', 'c')
+    file.write(f'{context}\n{declaration}\n\n')
 
-def callback_name(info):
-    args = ''.join([arg["type_name"] for arg in info["args"]]);
-    return f'{info["return_expr"]["type_name"]}_{args}';
+# Generates a name for a callback wrapper based on it's type
+def callbackWrapperName(info):
+    return_ = info["return_expr"]["type_name"]
+    args = ''.join( [arg["type_name"] for arg in info["args"]] )
+    return f'{return_}_{args}'
 
-def callback_wrapper(info, name):
-    args = [f'{arg["type_name"]} arg{i+1}' for i, arg in enumerate(info["args"])];
-    content = Block([FunctionCall('lua_pushvalue', ['c.stack', 'c.idx'], semicolon=True)]);
+# Generates a wrapper that allows to callback to lua functions
+def callbackWrapper(info, name):
+    args = [f'{arg["type_name"]} arg{i+1}' for i, arg in enumerate(info["args"])]
+    prefix = Block([
+        FunctionCall('lua_pushvalue', ['c.stack', 'c.idx'], semicolon=True)
+    ])
     for i in range(len(args)):
-        f = FunctionCall('lua_pushinteger', ['c.stack', f'arg{i+1}'], semicolon=True);
-        content = content + f;
-    pcall = FunctionCall('lua_pcall', ['c.stack', len(args), 1, 0], semicolon=True);
-    pop = FunctionCall('lua_tonumber', ['c.stack', -1])
-    content = content + pcall + Return(pop);
-    return Function(info["return_expr"]["type_name"], name, args, content=content);
+        push = FunctionCall(
+            'lua_pushinteger', 
+            ['c.stack', f'arg{i+1}'], 
+            semicolon=True
+        )
+        prefix = prefix + push
+    pcall = FunctionCall(
+        'lua_pcall', 
+        ['c.stack', len(args), 1, 0], 
+        semicolon=True
+    )
+    pop = FunctionCall(
+        'lua_tonumber', 
+        ['c.stack', -1]
+    )
+    content = prefix + pcall + Return(pop)
+    return Function(info["return_expr"]["type_name"], name, args, content=content)
 
-def write_wrappers(file, data):
+# Writes wrappers for API functions and required callback types to file
+def writeWrappers(file, data):
     for function in data:
-        argcount = len(function["args"]);
+        argcount = len(function["args"])
 
-        setup = [];
-        apiargs = [];
+        setup, apiargs = [], []
         for i, arg in enumerate(function["args"]):
             if arg["type"] == 'fptr_type':
-                name = callback_name(arg["info"]);
-                if name not in callbacks:
-                    callbacks[name] = True;
-                    wrapper = callback_wrapper(arg["info"], name);
-                    file.write(str(wrapper) + '\n\n');
-                apiargs = apiargs + [f'&{name}'];
-                setup = setup + [ContextChange(i+1)];
+                name = callbackWrapperName(arg["info"])
+                if name not in usedWrapperNames:
+                    usedWrapperNames[name] = True
+                    wrapper = callbackWrapper(arg["info"], name);
+                    file.write(f'{wrapper}\n\n')
+                apiargs.append(f'&{name}')
+                setup.append(ContextChange(i+1));
             else:
                 x = Variable(
                     "int",
                     f'arg{i+1}',
-                    array=False,
                     value=FunctionCall('lua_tonumber', ['L', i+1])
-                );
-                apiargs = apiargs + [f'arg{i+1}']
-                setup = setup + [x];
-        prefix = Block(setup);
+                )
+                apiargs.append(f'arg{i+1}')
+                setup.append(x)
+        prefix = Block(setup)
 
         apicall = FunctionCall(
             function["name"],
             apiargs
-        );
+        )
 
-        if function['return_expr']['type_name'] == 'int':
-            suffix = Block([
-                FunctionCall('lua_pushinteger', ['L', apicall], semicolon=True),
-                Return(1)
-            ]); 
-        else:
-            apicall.semicolon = True;
-            suffix = Block([
-                apicall,
-                Return(0)
-            ]); 
+        suffix = Block([
+            FunctionCall('lua_pushinteger', ['L', apicall], semicolon=True),
+            Return(1)
+        ]) 
 
         wrapper = Function(
-            "int", 
+            'int', 
             f'c_{function["name"]}', 
-            ["lua_State* L"],
+            ['lua_State* L'],
             content=prefix + suffix
-        );
+        )
 
-        file.write(str(wrapper) + '\n\n'); 
+        file.write(f'{wrapper}\n\n')
 
-def write_register(file, data):
-    xs = ['{' + f'"{fun["name"]}", c_{fun["name"]}' + '}' for fun in data];
-
-    register = LuaRegister("luareg", xs);
-    file.write(str(register) + '\n');
+def writeRegister(file, data):
+    xs = [f["name"] for f in data]
+    register = LuaRegister("luareg", xs)
+    file.write(f'{register}\n\n')
 
 def main():
     parser = argparse.ArgumentParser(
         prog="binding-generator",
         description="Generates binding file based on C API AST dump."
-    );
-    parser.add_argument("inputfile");
-    parser.add_argument("outputfile");
-    args = parser.parse_args();
+    )
+    parser.add_argument("inputfile")
+    parser.add_argument("outputfile")
+    args = parser.parse_args()
 
-    data = load_info(args.inputfile);
+    data = loadInfo(args.inputfile)
     with open(args.outputfile, 'w') as output:
-        output.write("// Generated by binding-generator.py \n");
-        write_headers(output);
-        write_context(output);
-        write_declarations(output, data);
-        write_wrappers(output, data);
-        write_register(output, data);
-        output.write("\n// Boilerplate code \n");
-        write_boilerplate(output);
+        output.write("// Generated by binding-generator.py \n")
+        writeHeaders(output)
+        writeContext(output)
+        writeDeclarations(output, data)
+        writeWrappers(output, data)
+        writeRegister(output, data)
+        output.write("// Boilerplate code \n")
+        writeBoilerplate(output)
 
 if __name__ == '__main__':
-    main();
+    main()
--- src/binding-generator/components.py
+++ src/binding-generator/components.py
@@ -4,13 +4,11 @@ from typing import Optional
 from enum import Enum
 
 class Modifier(Enum):
-    CONST = 'const'
-    EXTERN = 'extern'
+    EXTERN='extern'
 
 class Component(ABC):
     pass
 
-# Include directive, ex. #include <stdio.h>
 @dataclass 
 class Include(Component):
     name: str 
@@ -18,67 +16,62 @@ class Include(Component):
 
     def __str__(self):
         if not self.system:
-            return f'#include "{self.name}"';
+            return f'#include "{self.name}"'
         else:
-            return f'#include <{self.name}>';
+            return f'#include <{self.name}>'
 
-# Code block, ex. { int x = 5; return x; }
 @dataclass 
 class Block(Component):
     content: list[Component]
 
     def __str__(self):
-        xs = map(str, self.content);
-        inside = '\n'.join(xs);
-        return '{\n' + inside + '\n}';
+        xs = map(str, self.content)
+        inside = '\n'.join(xs)
+        return '{\n' + inside + '\n}'
 
     def __add__(self, x):
         if not isinstance(x, Block):
-            x = Block([x]);
-        return Block(self.content + x.content);
+            x = Block([x])
+        return Block(self.content + x.content)
 
 @dataclass
 class LuaRegister(Component):
     name: str 
-    values: list[str]
+    f_names: list[str]
 
     def __str__(self):
-        declaration = f'const struct luaL_Reg {self.name}[]';
-        inside = ',\n'.join(self.values);
-        return declaration + ' = {\n' + inside + '\n};';
+        declaration = f'const struct luaL_Reg {self.name}[]'
+        xs = ['{' + f'{f_name}, c_{f_name}' + '}' for f_name in self.f_names]
+        return declaration + ' = {\n' + ',\n'.join(xs) + '\n};'
 
-# Variable declaration, ex. int x = 5;
 @dataclass
 class Variable(Component):
     type_: str
     name: str
     array: Optional[bool] = False
-    modifier: Optional[Modifier] = None
     value: Optional[any] = None
 
     def __str__(self):
-        mod = f'{self.modifier.value} ' if self.modifier else '';
-        declaration = mod + f'{self.type_} {self.name}';
+        declaration = f'{self.type_} {self.name}'
         if self.array:
-            declaration = declaration + '[]';
-        return declaration + (f' = {self.value};' if self.value else ';');
+            declaration = declaration + '[]'
+        return declaration + (f' = {self.value};' if self.value else ';')
 
-# Function declaration, ex. int foo(int n) { return n+1; }
 @dataclass
 class Function(Component):
     type_: str 
     name: str 
     args: list[str]
-    modifier: Optional[Modifier] = None
+    modifier: Optional[any] = None
     content: Optional[Block] = None
 
     def __str__(self):
-        mod = f'{self.modifier.value} ' if self.modifier else '';
-        declaration = mod + f'{self.type_} {self.name}({", ".join(self.args)})';
+        mod = f'{self.modifier.value} ' if self.modifier else ''
+        declaration = mod + f'{self.type_} {self.name}({", ".join(self.args)})'
         if self.content:
-            return f'{declaration} {self.content}';
+            return f'{declaration} {self.content}'
         else:
-            return f'{declaration};';
+            return f'{declaration};'
 
 @dataclass
 class FunctionCall(Component):
@@ -87,60 +80,43 @@ class FunctionCall(Component):
     semicolon: Optional[bool] = False
 
     def __str__(self):
-        xs = map(str, self.args);
-        return f'{self.name}({", ".join(xs)})' + (';' if self.semicolon else '');
+        xs = map(str, self.args)
+        return f'{self.name}({", ".join(xs)})' + (';' if self.semicolon else '')
 
 @dataclass
 class Return(Component):
     value: any 
 
     def __str__(self):
-        return f'return {self.value};';
-
-# struct declaration, ex. struct context { int idx; char c; };
-@dataclass 
-class Struct(Component):
-    name: str 
-    fields: list[Variable]
-
-    def __str__(self):
-        prefix = f'struct {self.name} ';
-        xs = map(str, self.fields);
-        content = '\n'.join(xs);
-        return prefix + '{\n' + content + '\n};'
+        return f'return {self.value};'
 
 @dataclass 
 class ContextChange(Component):
     idx: int 
 
     def __str__(self):
-        return f'c.stack = L; c.idx = {self.idx};';
-
-class Blank(Component):
-    def __str__(self):
-        return '';
-        
-@dataclass
-class Sequence():
-    content: list[Component]
-
-    def __add__(self, xs: Component | list[Component]):
-        if isinstance(xs, Component):
-            xs = [xs];
-        return Sequence(self.content + xs);
-
-    def __str__(self):
-        output = '';
-        for component in self.content:
-            output = str(component) + '\n';
-        return output;
+        return f'c.stack = L; c.idx = {self.idx};'
 
 @dataclass 
 class FunctionPointer(Component):
-    return_type: str 
+    type_: str 
     name: str 
     args: list[str]
 
     def __str__(self):
-        xs = ', '.join(self.args);
-        return f'{self.return_type} (*{self.name})({xs})';
+        xs = ', '.join(self.args)
+        return f'{self.type_} (*{self.name})({xs})'
+
+@dataclass 
+class Struct(Component):
+    name: str 
+    fields: list[Variable]
+
+    def __str__(self):
+        prefix = f'struct {self.name}'
+        content = '\n'.join(map(str, self.fields))
+        return prefix + '{\n' + content + '\n}'
+
+@dataclass 
+class Context(Struct):
+    pass

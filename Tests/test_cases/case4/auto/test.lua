2ee3522d1314bb1f7dde39d7fa0865572d2f43f1
Piotrtraczynski1 <piotr.traczynski1@gmail.com>
2024-05-12 17:14:50 +0200

    Fixed variable names in Lua table

--- Tests/test_cases/case4/auto/test.lua
+++ Tests/test_cases/case4/auto/test.lua
@@ -1,11 +1,13 @@
 function numerator(xs)
-    return xs[1]
+    return xs["x"]
 end
 
 function test_numerator()
     for i = 0, 100 do
         for j = 1, 100 do
-            xs = {i, j}
+            xs = {}
+            xs["x"] = i
+            xs["y"] = j
             if CFunction.numerator(xs) ~= numerator(xs) then
                 print("Test failed for numerator function, input: i = " .. i .. ", j = " .. j)
                 print("Result: " .. CFunction.numerator(xs) .. ", expected: " .. numerator(xs))
@@ -17,13 +19,15 @@ function test_numerator()
 end
 
 function multiply(a, b)
-    return a[1] * b
+    return a["val"] * b
 end
 
 function test_multiply()
     for i = 1, 1000 do
         for j = 100, 2000 do
-            if CFunction.multiply({i}, j) ~= multiply({i}, j) then
+            arr = {}
+            arr["val"] = i
+            if CFunction.multiply(arr, j) ~= multiply(arr, j) then
                 print("Test failed for multiply function, input: i = " .. i .. ", j = " .. j)
                 return false
             end

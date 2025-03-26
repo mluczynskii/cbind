0daf313dc2b65206dc0c96ce646abeea71af5a18
Mateusz Łuczyński <mateusz.luczynski02@gmail.com>
2025-03-26 17:39:39 +0100

    Adjust testing tools

--- tests/validate.sh
+++ tests/validate.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-gcc -c src/api.c -I./include -fdump-tree-original-raw -o api.o
-python3 ../cbind/parser.py api.c.005t.original
-python3 ../cbind/main.py dump.json -o src/binding.c
-make main
-make clean
+make main 2>/dev/null 1>/dev/null
 
-# TODO: loop over cases
-./main scripts/case_1.lua
-if [[ "$?" -eq 0 ]]; then 
-  echo "Test passed!"
-  exit 0
-else 
-  echo "Test failed..."
+PASSED=0
+TOTAL=0
+for filename in scripts/*.lua; do
+  [[ -e "$filename" ]] || continue
+  echo "Running case $filename..."
+  TOTAL=$((TOTAL + 1))
+  ./main "$filename"
+  if [[ "$?" -eq 0 ]]; then 
+    echo -e "\033[32mPassed!\033[0m"
+    PASSED=$((PASSED + 1))
+  else 
+    echo -e "\033[31mFailed...\033[0m"
+  fi 
+done
+
+make clean distclean 2>/dev/null 1>/dev/null
+
+if [[ "$PASSED" -ne "$TOTAL" ]]; then 
+  echo -e "Passed $PASSED/$TOTAL tests... 🤕🤕"
   exit 1
-fi 
+else
+  echo -e "\n🎉🎊🥳🎈🎂🎁💃🕺🏆✨🎆🎇"
+  echo "Every test passed!"
+  echo "🎉🎊🥳🎈🎂🎁💃🕺🏆✨🎆🎇"
+  exit 0
+fi
+
